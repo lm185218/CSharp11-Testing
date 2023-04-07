ee0780c0f4f6181fb4bea0e7ac6fec158d72d39c
Lewis McKaig <lm185218@ncr.com>
2023-04-07 11:34:32 +0100

    Add project files.

---
+++ CSharp11/GenericMathSupport.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace CSharp11
+{
+    static class GenericMathSupport
+    {
+        //this method calls a generic Square All method to demonstrate the new generic math support in c#11
+        public static void NewMathChanges()
+        {
+            double[] doubles = new[] { 3.9, 2.2, 8.6, 4.0, 0.0 };
+            var squaredDoubles = SquareAll(doubles);
+            foreach (var d in squaredDoubles)
+            {
+                Console.Write($"{d},");
+            }
+            Console.WriteLine();
+
+            int[] ints = new int[] { 0, 1, 2, 3 };
+            var squaredInts = SquareAll(ints);
+            foreach (var i in squaredInts)
+            {
+                Console.Write($"{i},");
+            }
+            Console.WriteLine();
+        }
+
+        // generic SquareAll method that lets us have one method for any type that inherits INumber<T>
+        private static T[] SquareAll<T>(T[] values)
+           where T : INumber<T>
+        {
+            List<T> squared = new List<T>();
+            foreach (var v in values)
+            {
+                //INumber contains a definition for zero that all implenting types must specify. Since 0 isnt a valid double and 0.0 isnt a valid int
+                var result = v.Equals(T.Zero) ? T.Zero : v * v;
+
+                squared.Add(result);
+            }
+
+            return squared.ToArray();
+        }
+    }
+}
---
+++ CSharp11/Program.cs
@@ -0,0 +1,93 @@
+﻿//Top level statements mean I can write code in the Program.cs without a surrounding class
+
+using CSharp11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*StringUpdates();*/
+        GenericMathSupport.NewMathChanges();
+    }
+
+    public static void StringUpdates()
+    {
+        //--------------------------------------------------------------------------------------------------
+
+        //New for c#11 is string literals, denoted by 3 double quotes (")
+        string stringLiteral = """
+        {
+         "Logging": {
+            "LogLevel":{
+                "Default": "Information"
+            }
+         }
+        }
+        """;
+        Console.WriteLine(stringLiteral);
+
+
+        //--------------------------------------------------------------------------------------------------
+        // string literals also support interpolation
+
+        var option = 2;
+
+        //new pattern matching syntax switch statements
+        var loggingValue = option switch
+        {
+            1 => "Information",
+            2 => "Debug",
+            3 => "Info",
+            _ => throw new ArgumentOutOfRangeException("option") // "_" represents the default
+        };
+
+        // old style switch statement, more verbose. 
+        /*switch (option)
+        {
+            case 1:
+                loggingValue = "Information";
+                break;
+            case 2:
+                loggingValue = "Debug";
+                break;
+            case 3:
+                loggingValue = "Info";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("option")
+        }*/
+
+        //using a double dollar sign on a literal string allows interpolation using double braces "{{}}"
+        string stringLiteralInterpolation = $$"""
+        {
+            "Logging": {
+            "LogLevel":{
+                "Default": "{{loggingValue}}"
+            }
+            }
+        }
+        """;
+
+        Console.WriteLine(stringLiteralInterpolation);
+
+        // you can even go overboard with the interpolation as they now support multi-line statements
+        // but I feel this is borderline criminal. Might have a use somewhere.
+        string multiLineInterpolation = $$"""
+        {
+         "Logging": {
+            "LogLevel":{
+                "Default": "{{option switch
+        {
+            1 => "Information",
+            2 => "Debug",
+            3 => "Info",
+            _ => throw new ArgumentOutOfRangeException("option") // "_" represents the default
+        }}}"
+            }
+         }
+        }
+        """;
+
+        Console.WriteLine(multiLineInterpolation);
+    }
+}
