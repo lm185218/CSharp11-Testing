02fd4855b3ccc4cfb44ee6011a9a9f8798a03ece
Lewis McKaig <lm185218@ncr.com>
2023-04-07 11:35:31 +0100

    uncomment

--- CSharp11/Program.cs
+++ CSharp11/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        /*StringUpdates();*/
+        StringUpdates();
         GenericMathSupport.NewMathChanges();
     }
 
